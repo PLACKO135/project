f9aea0bd6519a90505d96581af5475180d0a8889
Papp László Levente <papp.laszlo.levente@burg.lo.hu>
2024-09-12 09:44:38 +0200

    all element

--- function.js
+++ function.js
@@ -1,7 +1,9 @@
 function getFirstElement (arr){
     //ide írd kódod
-    return arr[0];
+    for (let i = 0; i < arr.length; i++){
+        return arr[i];
     }
+}
     const numbers = [1, 2, 3];
     const words = ["apple", "banana", "cherry"];
     console.log(`First number: ${getFirstElement(numbers)}`);
