83568e8a498f1aa87c1e89adc2baea354205b6c3
Papp László Levente <papp.laszlo.levente@burg.lo.hu>
2024-09-12 09:42:48 +0200

    Util function factorial

--- function.js
+++ function.js
@@ -11,3 +11,15 @@ function getFirstElement (arr){
     const words = ["apple", "banana", "cherry"];
     console.log(`First number: ${getFirstElement(numbers)}`);
     console.log(`First word: ${getFirstElement(words)}`);
+
+    export function factorial(n) {
+        //ide írd a kódod
+        if (n === 0 || n === 1) {
+          return 1;
+        } else {
+          return n * factorial(n - 1);
+        }
+    }
+        
+        console.log(`Factorial of 5: ${factorial(5)}`);
+        
